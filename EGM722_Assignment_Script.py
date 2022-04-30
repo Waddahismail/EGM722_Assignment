266b946ce6d5c2dc7e5d2cfd18171d2adffcbedd
Waddahismail <101291965+Waddahismail@users.noreply.github.com>
2022-05-01 00:18:54 +0100

    Roads Buffer
    Added an option to export the roads buffers as shapefiles

--- EGM722_Assignment_Script.py
+++ EGM722_Assignment_Script.py
@@ -1,3 +1,4 @@
+import geopandas
 import geopandas as gpd
 import cartopy.crs as ccrs
 import matplotlib.pyplot as pplt
@@ -5,82 +6,81 @@ from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 from matplotlib_scalebar.scalebar import ScaleBar
 
-
 #-------Set the data path------------------------------------------------------#
 data_path = 'D:\\_MSc\\EGM722\\Github\\EGM722_Assignment\\data_files'
 
 #Import the shapefiles as GeoPandas Geodataframe
-land = gpd.read_file(data_path+'\\land.shp')
+property = gpd.read_file(data_path + '\\properties.shp')
 rcv = gpd.read_file(data_path+'\\receivers.shp')
 roads = gpd.read_file(data_path+'\\roads.shp')
 
 #Display the EPSG Code of the data, and check if all has same projection
-if land.crs == rcv.crs == roads.crs:
-    print ('All features are of coordinate system {}'.format(land.crs))
+if property.crs == rcv.crs == roads.crs:
+    print ('All features are of coordinate system {}'.format(property.crs))
     pass
 else:
     print ('Data was reprojected as 1 or more features had mismatching projection')
-    land = land.to_crs(epsg=32639)
+    property = property.to_crs(epsg=32639)
     roads = roads.to_crs(epsg=32639)
     rcv = rcv.to_crs(epsg=32639)
-    print('Land Feature projection:{}\nRoads Feature Projection:{}\nReceivers Feature Projection:{}'
-               .format(land.crs,roads.crs,rcv.crs))
+    print('Properties Feature projection:{}\nRoads Feature Projection:{}\nReceivers Feature Projection:{}'
+          .format(property.crs, roads.crs, rcv.crs))
 
-#-------Count number of receiver points, and number of lands-------------------#
-print('\nThere are ({}) land parcels within the project\'s area'.format(land['geometry'].count()))
+#-------Count number of receiver points, and number of properties-------------------#
+print('\nThere are ({}) properties within the project\'s area'.format(property['geometry'].count()))
 print('There are total ({}) receiver points within the project\'s area\n'.format(rcv['geometry'].count()))
 
-#-------Check if receviers Intersect  with lands-------------------------------#
+#-------Check if receviers Intersect  with properties-------------------------------#
 def point_inside_shape(point, polygon):
-    """The function checks if the receivers intersect with land parcels.
+    """The function checks if the receivers intersect with properties.
 
-     The function intersects the land & receivers datasets,
-     and count how many are inside each property/land"""
+     The function intersects the properties & receivers datasets,
+     and count how many are inside each property"""
 
     global pt_poly_join
-    pt_poly_join = gpd.sjoin(land, rcv, how='inner', lsuffix='left', rsuffix='right')
+    pt_poly_join = gpd.sjoin(property, rcv, how='inner', lsuffix='left', rsuffix='right')
     pt_poly_count = pt_poly_join['geometry'].count()
 
     if pt_poly_count==0:
         print('No receivers within the properties')
     else:
-        print('Below are the number of receivers intersecting each land parcel:')
+        print('Below are the number of receivers intersecting each properties:')
         print(pt_poly_join.groupby(['land_id','land_type'])['station'].count())
 
-point_inside_shape(rcv,land)
+point_inside_shape(rcv, property)
 
-#--------Clip receivers with land layer & create clipped layer------------------#
-land_clip = gpd.clip(land, rcv)
-land_clip.insert(len(land_clip.columns), 'no_of_rcv', 0)
-land_clip.insert(len(land_clip.columns), 'compensation', 0)
+#--------Clip receivers with properties layer & create clipped layer------------------#
+properties_clipped = gpd.clip(property, rcv)
+properties_clipped.insert(len(properties_clipped.columns), 'no_of_rcv', 0)
+properties_clipped.insert(len(properties_clipped.columns), 'compensation', 0)
 
 #Update the number of receivers column
-for i in land_clip.iterrows():
-    land_clip['no_of_rcv'] = pt_poly_join.groupby(['land_id'])['station'].count()
+for i in properties_clipped.iterrows():
+    properties_clipped['no_of_rcv'] = pt_poly_join.groupby(['land_id'])['station'].count()
 
 #--------Calculate Compnsation per each property-------------------------------#
 def compensation(x,y):
-    """The function calculates the compensation per each land/property.
+    """The function calculates the compensation per each property.
 
-    The user inputs the compensation value for agricultural & residential lands,
+    The user inputs the compensation value for agricultural & residential properties,
     the function then calculates the value of compensation according to the number of receivers
     inside each property and updates the Compensation field, outputs result as csv"""
 
-    print('\nThe compensation values for each land:')
+    print('\nThe compensation values for each property:')
     #Delete NaN values rows
-    land_clip.dropna(inplace=True)
+    properties_clipped.dropna(inplace=True)
 
     #Calculate compensation per land type
-    land_clip.loc[land_clip['land_type'] == 'agricultural', 'compensation'] = land_clip['no_of_rcv'] * x
-    land_clip.loc[land_clip['land_type'] == 'residential', 'compensation'] = land_clip['no_of_rcv'] * y
+    properties_clipped.loc[properties_clipped['land_type'] == 'agricultural', 'compensation'] = properties_clipped['no_of_rcv'] * x
+    properties_clipped.loc[properties_clipped['land_type'] == 'residential', 'compensation'] = properties_clipped['no_of_rcv'] * y
 
-    print(land_clip , '\n')
+    print(properties_clipped, '\n')
 
     #Create a csv file of all compensation values per property
-    land_clip[['land_id', 'owner', 'compensation']].to_csv('Compensation.csv')
+    properties_clipped[['land_id', 'owner', 'compensation']].to_csv('Compensation.csv')
 
-compensation(int(input('\nEnter Agricultural land compensation value per sensor:')),
-             int(input('Enter Residential land compensation value per sensor:')))
+compensation(int(input('\nEnter Agricultural property compensation value per sensor:')),
+             int(input('Enter Residential property compensation value per sensor:')))
 
 #--------Buffer roads and clip receivers---------------------------------------#
 def rcv_skips():
@@ -89,6 +89,8 @@ def rcv_skips():
     Creates a list of all Receivers within certain distance of roads according to
     road type, and adds the result to a list, then create a Geodataframe and saves it as
     shapefile"""
+    global main_roads_buffer
+    global secondary_roads_buffer
 
     skipped_rcv = []
     dist = []
@@ -97,7 +99,15 @@ def rcv_skips():
     dist1 = int(input('Enter the distance to be skipped around Main roads:'))
     dist2 = int(input('Enter the distance to be skipped around Secondary roads:'))
 
-    #Iterate through road types and assign skip distances
+    #Create main and secondary roads buffer features
+    main_roads = roads[roads['road_type'] == 'main']
+    main_roads_buffer = main_roads.geometry.buffer(dist1)
+    main_roads_buffer.to_file('main_roads_buffer')
+    secondary_roads = roads[roads['road_type'] == 'secondary']
+    secondary_roads_buffer = secondary_roads.geometry.buffer(dist2)
+    secondary_roads_buffer.to_file('secondary_roads_buffer')
+
+    #Iterate through road types and assign skip distances to a list
     for rd_type in roads['road_type']:
         if rd_type == 'main':
             dist.append(dist1)
@@ -126,10 +136,12 @@ ax = pplt.axes(projection=ccrs.Mercator())
 
 #Plot the data
 pplt.title("Land Parcels & Skipped Receivers", fontsize=20, color="black")
-land.plot(ax=ax, alpha = 0.5, color = 'blue', label = 'Land', transform = myCRS)
-rcv.plot(ax=ax,color = 'green', label = 'Receivers', transform = myCRS)
-skipped_rcv_gdf.plot(ax=ax,color = 'red', label = 'Skipped Receivers', transform = myCRS)
-roads.plot(ax=ax, color = 'orange', label = 'Roads', transform = myCRS)
+property.plot(ax=ax, alpha = 0.5, color ='blue', transform = myCRS)
+main_roads_buffer.plot(ax=ax, alpha = 0.5, color ='red', transform = myCRS)
+secondary_roads_buffer.plot(ax=ax, alpha = 0.5, color ='yellow', transform = myCRS)
+rcv.plot(ax=ax,color = 'green', transform = myCRS)
+skipped_rcv_gdf.plot(ax=ax,color = 'red', transform = myCRS)
+roads.plot(ax=ax, color = 'orange', transform = myCRS)
 
 #Create Legend shapes
 legend_elements = [Line2D([0], [0], marker='o', color='w', label='Receiver',
@@ -138,7 +150,12 @@ legend_elements = [Line2D([0], [0], marker='o', color='w', label='Receiver',
                           markerfacecolor='r',markersize=10),
                    Line2D([0], [0], color='orange', lw=2, label='Roads'),
                    Patch(facecolor='blue', edgecolor='black',
-                         label='Land', alpha = 0.5)]
+                         label='Property', alpha = 0.5),
+                   Patch(facecolor='red', edgecolor='black',
+                         label='Main Roads Buffer', alpha=0.5),
+                   Patch(facecolor='yellow', edgecolor='black',
+                         label='Secondary Roads Buffer', alpha=0.5)
+                   ]
 
 ax.legend(handles=legend_elements, loc='upper right')
 
